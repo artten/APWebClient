cd968403d5a71d9ed643ea6316096c69ad734cbd
artten <artten12380@gmail.com>
2022-04-01 12:24:29 +0300

    added

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          Saar the kingssss
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
