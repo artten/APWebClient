e8cb706e74c741e7888e9b7be9271250eff3d068
artten <artten12380@gmail.com>
2022-04-21 12:36:38 +0300

    added texts

--- src/ChatApp.js
+++ src/ChatApp.js
@@ -1,19 +1,38 @@
-import React, { useState } from "react";
 import "./App.css";
+import { Button, Modal, Form } from "react-bootstrap";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import { useNavigate } from "react-router-dom";
+import React, { useRef, useState, useEffect } from "react";
 
 function ChatApp(props) {
-  const [newText, setNewText] = useState("")
+  const [newText, setNewText] = useState("");
+  const [otherUser, setOtherUser] = useState("");
+  const [textsToDisplay, setTextsToDisplay] = useState("");
+  function pText() {
+    var indents = [];
+    for (var i = 0; i < textsToDisplay.length; i++) {
+      if (textsToDisplay[i].name == "artiom") {
+        indents.push(<p id="login_user_text">{textsToDisplay[i].message}</p>);
+      } else
+        indents.push(<p id="other_user_text">{textsToDisplay[i].message}</p>);
+    }
+    return indents;
+  }
 
   function addText() {
-          props.chats[
-            props.users.findIndex(
-              (user) => user.userName == props.loginUser.loginUser
-            )
-          ]
+    setTextsToDisplay(
+      props.chats[
+        props.chats.findIndex(
+          (chat) =>
+            chat.recipients[0] == "artiom" && //props.loginUser &&
+            chat.recipients[1] == "sasr"
+        )
+      ].texts
+    );
   }
 
   return (
-    <body>
+    <div>
       <div class="container">
         <div class="leftSide">
           <div class="head">
@@ -34,22 +53,25 @@ function ChatApp(props) {
           </div>
         </div>
         <div class="rightSide">
-          <input
-            type="text"
-            id="input_text"
-            className="form-control"
-            placeholder="Text"
-            value={newText}
-            onChange={(e) => setNewText(e.target.value)}
-          />
-          <div id="input_text" class="input-group-append">
-            <button class="btn btn-primary" type="button" onClick={addText}>
-              Send
-            </button>
+          <div id="input_text">
+            {pText()}
+            <div class="input-group mb-3">
+              <input
+                type="text"
+                class="form-control"
+                aria-label="Recipient's username"
+                aria-describedby="basic-addon2"
+              />
+              <div class="input-group-append">
+                <button class="btn btn-primary" type="button" onClick={addText}>
+                  Send
+                </button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </body>
+    </div>
   );
 }
 
